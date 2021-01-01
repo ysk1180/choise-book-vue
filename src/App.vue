f71b781f75c0e151a3691c6978537cbe3d30906c
Yusuke Miyazaki <ysk1180@gmail.com>
2021-01-01 14:46:35 +0900

    navbarのデザインを整えた

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <nav class="bg-gray-800 h-14">
-      <div class="text-gray-200 text-xl font-bold p-3.5 float-left font-serif">
+    <nav class="bg-gray-800 h-14 flex">
+      <div class="text-gray-200 text-xl font-bold my-auto px-4 font-serif">
         <router-link to="/" class="flex">
           <img src="@/assets/img/book-icon.png" class="object-contain w-6" />
           <p class="pl-2">
@@ -9,7 +9,7 @@
           </p>
         </router-link>
       </div>
-      <div class="text-gray-300 px-3 py-4 float-right">
+      <div class="text-gray-300 pr-4 my-auto ml-auto">
         <router-link to="/about">
           これはなに？
         </router-link>
