a46373ed4c68c62c72bffbd91aadfedcfd7de24f
Yusuke Miyazaki <ysk1180@gmail.com>
2021-01-01 17:32:24 +0900

    Google Analytics追加

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css' // 参考:https://tailwindcss.com/docs/guides/vue-3-vite
+import VueGtag from "vue-gtag-next";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+if (process.env.VUE_APP_CURRENT_ENV === 'production') {
+  app.use(VueGtag, {
+    property: {
+      id: "G-PFY52GM213"
+    }
+  });
+}
+
+app.use(store).use(router).mount('#app')
