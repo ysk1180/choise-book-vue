8fd2ebade13a45166e4104d79ec9e1d7bff5230e
Yusuke Miyazaki <ysk1180@gmail.com>
2022-04-23 19:47:47 +0900

    トップページに候補のリンクを追加した

---
+++ src/components/RecommendWords.vue
@@ -0,0 +1,35 @@
+<template>
+  <div className="mx-4 my-2">
+    <h2 className="text-lg px-2 mb-1.5 font-bold inline-block" style="background: linear-gradient(transparent 60%, #8effa1 60%);">バックエンド</h2>
+    <div class="flex flex-wrap">
+      <div v-for="word in words" :key="word" class="m-1">
+        <router-link :to="`/search/${word}`">
+          <div class="flex border border-solid border-gray-300 rounded p-2 cursor-pointer hover:opacity-70">
+            <img :src="require(`@/assets/img/words/${word}.png`)" class="object-contain w-6 mr-1.5" />
+            {{word}}
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RecommendWords',
+  data(): {
+    words: string[];
+  }{
+    return {
+      words: [
+        'Ruby', // バックエンド
+        'Rails',
+        'PHP',
+        'Laravel',
+      ]
+    }
+  },
+});
+</script>
